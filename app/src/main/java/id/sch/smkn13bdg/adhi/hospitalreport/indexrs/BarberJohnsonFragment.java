1c5de676fd35e7e21023fb0c89c8a71db6c6d61a
adhimeong <adhimeong@gmail.com>
2017-12-08 23:20:14 +0700

    garfik barber jhonson

---
+++ app/src/main/java/id/sch/smkn13bdg/adhi/hospitalreport/indexrs/BarberJohnsonFragment.java
@@ -0,0 +1,30 @@
+package id.sch.smkn13bdg.adhi.hospitalreport.indexrs;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import id.sch.smkn13bdg.adhi.hospitalreport.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BarberJohnsonFragment extends Fragment {
+
+
+    public BarberJohnsonFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_barber_johnson, container, false);
+        // Inflate the layout for this fragment
+        return view;
+    }
+
+}
